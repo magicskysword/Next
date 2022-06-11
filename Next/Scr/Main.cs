87ea9451ee6727ffbde0a595627a86f04f59f203
MagicSkySword <skyswordkill@qq.com>
2022-06-11 13:20:45 +0800

    update 0.4.5 new save system

--- Next/Scr/Main.cs
+++ Next/Scr/Main.cs
@@ -14,7 +14,7 @@ namespace SkySwordKill.Next
     [BepInPlugin("skyswordkill.plugin.Next", "Next", MOD_VERSION)]
     public partial class Main : BaseUnityPlugin
     {
-        public const string MOD_VERSION = "0.4.4";
+        public const string MOD_VERSION = "0.4.5";
         
         public static Lazy<string> pathModsDir =
             new Lazy<string>(() => Utility.CombinePaths(
